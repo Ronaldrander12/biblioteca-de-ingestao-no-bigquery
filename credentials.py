70085e22540bb7f105aa8611895cf2259745c0f6
Ronald Rander <86743672+Ronaldrander12@users.noreply.github.com>
2022-01-24 15:46:45 -0300

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -10,19 +10,19 @@ class Credentials():
         self.client_secret = secretmanager.SecretManageServiceClient()
         self.path_cred = '--Caminho do arquivo de credenciais--'
 
-def set_environment_keys(self, key_type='main_key', path_key=False):
-    if key_type=='main_key':
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= self.path_cred
+    def set_environment_keys(self, key_type='main_key', path_key=False):
+        if key_type=='main_key':
+            os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= self.path_cred
 
-def access_secret_version(self, secret_id, version_id="latest"):
-    client = secretmanager.SecretManagerServiceClient()
-    gcp_project_id = self.client_secret.project
-    name = f"projects/{gcp_project_id}/secrets/{secret_id}/versions/{version_id}"
-    response = client.access_secret_version(name=name)
+    def access_secret_version(self, secret_id, version_id="latest"):
+        client = secretmanager.SecretManagerServiceClient()
+        gcp_project_id = self.client_secret.project
+        name = f"projects/{gcp_project_id}/secrets/{secret_id}/versions/{version_id}"
+        response = client.access_secret_version(name=name)
 
-    return response.payload.data.decode('UTF-8')
+        return response.payload.data.decode('UTF-8')
 
-def service_account_credentials(self, secret_id):
-    json_acct_info = json.loads(self.access_secret_version(secret_id))
-    
-    return service_account.Credentials.from_service_account_info(json_acct_info)
+    def service_account_credentials(self, secret_id):
+        json_acct_info = json.loads(self.access_secret_version(secret_id))
+
+        return service_account.Credentials.from_service_account_info(json_acct_info)
